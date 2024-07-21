2b2a61402f3b4ff9de30b5c12f200a4ac1eced81
John Valai <git@jvk.to>
2024-07-21 03:19:42 +0100

    chore: Cleanup

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
+
 const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx'
-  })
-   
-  module.exports = withNextra()
-   
-  // If you have other Next.js configurations, you can pass them as the parameter:
-  // module.exports = withNextra({ /* other next.js config */ })
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx'
+})
+  
+module.exports = withNextra()
+  
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
