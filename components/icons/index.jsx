02fd4ab1f3f34ce099be681620aadffe035726d2
John Valai <git@jvk.to>
2024-07-21 06:45:19 +0100

    chore: Added some demo contents

---
+++ components/icons/index.jsx
@@ -0,0 +1,4 @@
+export { default as LightningIcon } from './lightning.svg'
+export { default as GlobeIcon } from './globe.svg'
+export { default as StarsIcon } from './stars.svg'
+export { default as CardsIcon } from './cards.svg'
