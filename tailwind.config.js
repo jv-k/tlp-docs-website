fbee6557328a4d2cc2c584455037fe9dae7424f6
John Valai <git@jvk.to>
2024-07-21 06:51:31 +0100

    Chore: Added theme config to tailwind watch folders

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,md,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,md,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,md,mdx}",
- 
+     './theme.config.jsx',
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,md,mdx}",
   ],
