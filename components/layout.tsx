7322ba339e3f711a1957dc52a775147e36394f83
John Valai <git@jvk.to>
2024-07-24 02:34:40 +0100

    style: changed font to 'Inter Var'

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,21 +1,27 @@
-import { Roboto as FontSans } from "next/font/google"
- 
+import { Roboto } from "next/font/google"
+import { Inter } from "next/font/google"
+
 import { cn } from "@/lib/utils"
  
-const fontSans = FontSans({
+const fontSans = Roboto({
   weight: ['400', '700'],
   subsets: ["latin"],
   variable: "--font-sans",
 })
- 
+
+const fontInter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
 export default function Layout({ children }) {
   return (
     <>
       <main
         className={cn(
 "min-h-screen antialiased",
-          // font-sans
           // fontSans.variable
+          fontInter.variable
         )}
       >
         {children}
---
+++ tailwind.config.ts
@@ -0,0 +1,5 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+      fontFamily: {
+        // sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ['Inter var', ...fontFamily.sans],
+      },
